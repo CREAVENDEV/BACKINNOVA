1fddec6e1fc89a4e17685363f9983abe0147c901
Omar Crespo <omar.crespo@seconad.com>
2025-06-09 17:16:42 -0600

    Deploy 38 back

--- backend_proyecto/settings.py
+++ backend_proyecto/settings.py
@@ -148,3 +148,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 ###CORS_ALLOWED_ORIGINS = ['https://frontinnova.onrender.com/']
+
+ALLOWED_HOSTS = ['backinnova.onrender.com', 'localhost', '127.0.0.1']
